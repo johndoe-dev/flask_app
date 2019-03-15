60f6563541dad746c98c167bf34d2b9569e46ce5
Jonathan Ullindah <jonathan.ullindah@open-groupe.com>
2019-03-15 13:26:16 +0100

    Change FLASK_APP=app to FLASK_APP=flask_app.py

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # The FLASK_APP environment variable is used to specify how to load the application.
-export FLASK_APP=app
+export FLASK_APP=flask_app.py
 
 # Setting FLASK_ENV to development will enable debug mode
 export FLASK_ENV=development
